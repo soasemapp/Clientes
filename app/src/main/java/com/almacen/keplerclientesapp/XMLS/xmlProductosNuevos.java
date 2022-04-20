3f0fc6d99b4e9c1bbbe7c80108947b17b89ebf7d
jamcrabral <jamcrabral@gmail.com>
2022-04-20 08:54:00 -0500

    Create xmlProductosNuevos.java

---
+++ app/src/main/java/com/almacen/keplerclientesapp/XMLS/xmlProductosNuevos.java
@@ -0,0 +1,49 @@
+package com.almacen.keplerclientesapp.XMLS;
+
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.IOException;
+
+public class xmlProductosNuevos extends SoapSerializationEnvelope {
+
+    String usuario = "";
+    String clave = "";
+
+
+    public xmlProductosNuevos(int version) {
+        super(version);
+    }
+
+    public void xmlProductosNuevos(String usuario, String clave) {
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    @Override
+    public void write(XmlSerializer writer) throws IOException {
+        env = "http://schemas.xmlsoap.org/soap/envelope/";
+        String tem = "";
+        writer.startDocument("UTF-8", true);
+        writer.setPrefix("soap", env);
+        writer.setPrefix("", tem);
+        writer.startTag(env, "Envelope");
+        writer.startTag(env, "Body");
+        writer.startTag(tem, "ProdcutosNuevosRequest");
+        writer.startTag(tem, "Login");
+        writer.startTag(tem, "user");
+        writer.text(usuario);
+        writer.endTag(tem, "user");
+        writer.startTag(tem, "pass");
+        writer.text(clave);
+        writer.endTag(tem, "pass");
+        writer.endTag(tem, "Login");
+
+
+        writer.endTag(tem, "ProdcutosNuevosRequest");
+        writer.endTag(env, "Body");
+        writer.endTag(env, "Envelope");
+        writer.endDocument();
+
+    }
+}
